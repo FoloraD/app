35c94a51fdbd1392b5d12d9f09babf5acd4404a6
Sen <65850511+Senny10@users.noreply.github.com>
2023-10-14 12:04:49 +0100

    add timeline card

--- src/App.js
+++ src/App.js
@@ -1,30 +1,32 @@
-import logo from './logo.svg';
-import './App.css';
-import { useState } from 'react';
-import { Page1 } from './components/Page1';
-import { Page2 } from './components/Page2';
-import { Navbar } from './components/Navbar';
-import { Home } from './components/Home';
+import { useState } from "react";
+import "./App.css";
+import { Home } from "./components/Home";
+import { Navbar } from "./components/Navbar";
+import { Page1 } from "./components/Page1";
+import { Page2 } from "./components/Page2";
+import { TimelineCard } from "./components/TimelineCard";
+import logo from "./logo.svg";
 
 function SelectPage(state, setState) {
-  switch (state.page) {
-    case "page1":
-      return <Page1 state={state} setState={setState} />
-    case "page2":
-      return <Page2 state={state} setState={setState} />
-  }
+	switch (state.page) {
+		case "page1":
+			return <Page1 state={state} setState={setState} />;
+		case "page2":
+			return <Page2 state={state} setState={setState} />;
+	}
 }
 
 function App() {
-  const [state, setState] = useState({page: "page1"})
+	const [state, setState] = useState({ page: "page1" });
 
-  return (
-    <div className="App">
-     <Navbar/>
-      {SelectPage(state, setState)}
-      <Home/>
-    </div>
-  );
+	return (
+		<div className='App'>
+			<Navbar />
+			{SelectPage(state, setState)}
+			<Home />
+			<TimelineCard />
+		</div>
+	);
 }
 
 export default App;
---
+++ src/components/TimelineCard.js
@@ -0,0 +1,28 @@
+import React from "react";
+import { Chrono } from "react-chrono";
+
+export const TimelineCard = () => {
+	const items = [
+		{
+			title: "May 1940",
+			cardTitle: "Dunkirk",
+			url: "http://www.history.com",
+			cardSubtitle:
+				"Men of the British Expeditionary Force (BEF) wade out to..",
+			cardDetailedText:
+				"Men of the British Expeditionary Force (BEF) wade out to..",
+			media: {
+				type: "IMAGE",
+				source: {
+					url: "http://someurl/image.jpg",
+				},
+			},
+		},
+	];
+
+	return (
+		<div style={{ width: "500px", height: "400px" }}>
+			<Chrono items={items} />
+		</div>
+	);
+};
