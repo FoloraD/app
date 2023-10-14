4f421438b210e4d7404c21db20d772136afc0b8d
Adil Rahman <82183381+adil-rahman1@users.noreply.github.com>
2023-10-14 14:18:44 +0100

    added styling to top section of timeline

--- src/components/Timeline.js
+++ src/components/Timeline.js
@@ -4,7 +4,7 @@ export function Timeline() {
       <nav class="border-gray-200 bg-[#fff] dark:bg-gray-800 dark:border-gray-700">
         <div class="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
           <span class="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">
-            Here's your journey as a Footballer
+            Here's your journey as a scientist
           </span>
           <button
             data-collapse-toggle="navbar-solid-bg"
@@ -31,16 +31,22 @@ export function Timeline() {
           </button>
           <div class="hidden w-full md:block md:w-auto" id="navbar-solid-bg">
             <ul class="flex flex-col font-medium mt-4 rounded-lg bg-gray-50 md:flex-row md:space-x-8 md:mt-0 md:border-0 md:bg-transparent dark:bg-gray-800 md:dark:bg-transparent dark:border-gray-700">
-              <li>
+              <li class="flex flex-row">
+                <div class="text-white text-[32px] font-bold font-['Inter'] mr-2">
+                  📟
+                </div>
                 <a
-                  href="#"
+                  href="javascript:void(0)"
                   class="block py-2 pl-3 pr-4 text-white bg-blue-700 rounded md:bg-transparent md:text-blue-700 md:p-0 md:dark:text-blue-500 dark:bg-blue-600 md:dark:bg-transparent"
                   aria-current="page"
                 >
                   Past
                 </a>
               </li>
-              <li>
+              <li class="flex flex-row">
+                <div class="text-white text-[32px] font-bold font-['Inter'] mr-2">
+                  ☁️
+                </div>
                 <a
                   href="#"
                   class="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"
@@ -48,7 +54,10 @@ export function Timeline() {
                   Present
                 </a>
               </li>
-              <li>
+              <li class="flex flex-row">
+                <div class="text-white text-[32px] font-bold font-['Inter'] mr-2">
+                  🌠
+                </div>
                 <a
                   href="#"
                   class="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"
@@ -60,7 +69,11 @@ export function Timeline() {
           </div>
         </div>
       </nav>
-      <section></section>
+      <section>
+        <p class="border-gray-200 bg-[#fff] dark:bg-gray-800 dark:border-gray-700">
+          You are viewing your past
+        </p>
+      </section>
     </>
   );
 }
