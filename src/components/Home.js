701aa8e8e73f5ac5ea6f04d091a7abaeed538bde
aburathoki1 <asha.burathoki@madetech.com>
2023-10-14 14:06:58 +0100

    add icon to button

--- src/components/Home.js
+++ src/components/Home.js
@@ -17,7 +17,7 @@ export function Home() {
       <input className='text-white px-10 py-2 shadow-lg' aria-label='search input' placeholder="a scientist..."></input>
 
      
-      <button className='bg-[#008364] px-12 text-[#FFFFFF] font-bold  py-2 shadow-lg'> search </button>
+      <button className='bg-[#46F0BD] px-12 text-black font-bold  py-2 shadow-lg'>☁️ search </button>
 
       </div>
 
